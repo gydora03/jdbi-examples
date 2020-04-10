e909c521c8a3467737a998f5333d52cf61a55db8
György Dóra <gyorgydora99@gmail.com>
2020-04-10 15:17:49 +0200

    Added files

---
+++ src/main/java/user/Main.java
@@ -0,0 +1,58 @@
+package user;
+
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Jdbi jdbi = Jdbi.create("jdbc:h2:mem:test");
+        jdbi.installPlugin(new SqlObjectPlugin());
+
+        try (Handle handle = jdbi.open()) {
+            UserDao dao = handle.attach(UserDao.class);
+            dao.createTable();
+
+            User user1 = User.builder()
+                    .name("James Bond")
+                    .username("007")
+                    .password("secret")
+                    .email("jamesbond@something.com")
+                    .gender(User.Gender.MALE)
+                    .dob(LocalDate.parse("1920-11-11"))
+                    .enabled(true)
+                    .build();
+
+            User user2 = User.builder()
+                    .name("Sherlock Holmes")
+                    .username("holmes")
+                    .password("secret")
+                    .email("sherlockholmes@something.com")
+                    .gender(User.Gender.MALE)
+                    .dob(LocalDate.parse("1854-01-06"))
+                    .enabled(true)
+                    .build();
+
+            dao.insertUser(user1);
+            dao.insertUser(user2);
+
+            dao.list().stream().forEach(System.out::println);
+
+            System.out.println(dao.findById(1).get().toString());
+
+            System.out.println(dao.findByUsername("holmes").get().toString());
+
+            dao.delete(user2);
+            dao.list().stream().forEach(System.out::println);
+
+            /*dao.insertUser(user1);
+            dao.findById(1).ifPresent(System.out::println);
+            dao.findByUsername("007").ifPresent(System.out::println);
+            dao.list().stream().forEach(System.out::println);
+            dao.delete(user1);*/
+        }
+    }
+}
